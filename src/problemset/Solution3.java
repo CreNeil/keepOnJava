11a7d0dfe5697f3c7f304bb0b71109a2e924b1ea
CreNeil <Chenguang_Nie@outlook.com>
2021-06-05 21:52:32 +0800

    (Problem #000001) 1.dp

--- src/problemset/Solution3.java
+++ src/problemset/Solution3.java
@@ -6,6 +6,8 @@ public class Solution3 {
     public static void main(String[] args) {
         System.out.println(translateNum(12258));
         System.out.println(translateNum2(12258));
+        System.out.println(climbStairs(5));
+        System.out.println(numDecodings2("226"));
     }
 
     //bad 还可以优化
@@ -43,6 +45,86 @@ public class Solution3 {
             }
         }
         return r;
+    }
+
+    // 70
+    public static int climbStairs(int n) {
+        int rnj2, rnj1 = 0;
+        int rn = 1;
+
+        for (int i = 0; i < n; i++) {
+            rnj2 = rnj1;
+            rnj1 = rn;
+            rn = rnj1 + rnj2;
+        }
+        return rn;
+    }
+
+    //91
+    public static int numDecodings(String s) {
+        int[] f = new int[s.length() + 1];
+        f[0] = 1;
+        for (int i = 1; i <= s.length(); i++) {
+            if (s.charAt(i - 1) != '0') {
+                f[i] += f[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                f[i] += f[i - 2];
+            }
+        }
+        return f[s.length()];
+    }
+
+    public static int numDecodings2(String s) {
+        int fn2 = 0, fn1 = 1, fn = 0;
+        for (int i = 1; i <= s.length(); i++) {
+            fn = 0;
+            if (s.charAt(i - 1) != '0') {
+                fn += fn1;
+            }
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                fn += fn2;
+            }
+            fn2 = fn1;
+            fn1 = fn;
+        }
+        return fn;
+    }
+
+
+    public static int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] f = new int[length];
+        f[0] = nums[0];
+        f[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            f[i] = Math.max(f[i - 2] + nums[i], f[i - 1]);
+        }
+        return f[length - 1];
+    }
+
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int fn2 = nums[0];
+        int fn1 = Math.max(nums[0], nums[1]);
+        int fn = fn1;
+        for (int i = 2; i < nums.length; i++) {
+            fn = Math.max(fn2 + nums[i], fn1);
+            fn2 = fn1;
+            fn1 = fn;
+        }
+        return fn;
     }
 
 }
